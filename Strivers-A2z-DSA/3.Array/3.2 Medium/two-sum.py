deb8aea833454e2f2468d9015195c4ef9afde0b6
Ujjwal Kumar <ukt1997@gmail.com>
2022-09-23 00:02:29 +0530

    Array Medium Q1-Q3

---
+++ Strivers-A2z-DSA/3.Array/3.2 Medium/two-sum.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dic.keys():
+                return sorted([i, dic[target - nums[i]]])
+            else:
+                dic[nums[i]] = i
+
+
