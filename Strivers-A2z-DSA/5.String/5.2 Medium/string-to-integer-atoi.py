b3208935d475a1e2fb2a36a408b96540a50f11e1
Ujjwal Kumar <ukt1997@gmail.com>
2022-10-08 16:53:44 +0530

    Staring Medium Q4

--- Strivers-A2z-DSA/5.String/5.2 Medium/string-to-integer-atoi.py
+++ Strivers-A2z-DSA/5.String/5.2 Medium/string-to-integer-atoi.py
@@ -1,26 +1,47 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def myAtoi(self, s: str) -> int:
+        # return 12
         n = len(s)
-        max_val = s[0]
-        max_len = 1
-        for i in range(n):
-            for j in range(i + 1, n):
-                chk = True
-                start = i
-                end = j
-                # print(start,end)
-                while start < end:
-                    # print("In Loop ",start , end)
-                    if s[start] != s[end]:
-                        chk = False
-                    start += 1
-                    end -= 1
-                if chk and max_len < (j - i + 1):
-                    # print("Inside IF check")
-                    max_len = j - i + 1
-                    max_val = s[i:j + 1]
-        return max_val
+        start = 0
 
+        # Removed all Leading <Spaces>
+        positive = True
+        while start < n and s[start] == ' ':
+            start += 1
+
+        # Checking if -ve Number
+        if start < n and s[start] == '-':
+            # print(start," Here - ")
+            positive = False
+            start += 1
+            if start < n and (s[start] < '0' or s[start] > '9'):
+                return 0
+        elif start < n and s[start] == '+':
+            # print(start," Here + ")
+            start += 1
+            # print(s[start])
+            if start < n and (s[start] < '0' or s[start] > '9'):
+                return 0
+        elif start < n and (s[start] < '0' or s[start] > '9'):
+            print(start, " Here return ")
+            return 0
+
+        end = start
+        while end < n and s[end] >= '0' and s[end] <= '9':
+            end += 1
+
+        # print(start,end)
+        if start < n:
+            val = int(s[start])
+            for i in range(start + 1, end):
+                val = val * 10 + int(s[i])
+                print(val)
+            if positive:
+                return min(val, 2147483647)
+            else:
+                return max(-1 * val, -2147483648)
+
+        return 0
 
 
 
