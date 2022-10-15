4449de53c6b58e9e7d40e7d747f174a997d00ef5
Ujjwal Kumar <32262314+ukt1997@users.noreply.github.com>
2022-10-15 16:34:15 +0530

    Time: 3803 ms (45.40%), Space: 369.6 MB (31.29%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -3,13 +3,17 @@ class Solution:
         @cache
         def inner_func(start, prev, last_count, K_left):
             if K_left < 0:
+                # terminating conditin when K is over 
                 return float("inf") 
             if start >= len(s):
+                # terminating conditin when Start pointer passed whole string 
                 return 0
             if s[start]==prev:
+                # will add cur char to the string , We are not trying skipping it here as this we have already tried while selecting the 1st occurance of this char , No point in adding 1st a out of aaa and skipiing any of 2nd or 3rd in all these scenarios resultent will be aa only . 
                 incr = 1 if last_count in (1,9,99) else 0 
                 return incr + inner_func(start+1, prev, last_count+1, K_left) 
             else:
+                # will check if we need to keep this char or skip this and go with min of that 
                 keep = 1  + inner_func(start+1, s[start], 1, K_left) 
                 delete = inner_func(start+1, prev, last_count, K_left-1) 
                 return min(keep, delete)
