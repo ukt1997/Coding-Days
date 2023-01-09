9c66d900384fd1db2bf074977197c514ce4778e7
Ujjwal Kumar <32262314+ukt1997@users.noreply.github.com>
2023-01-09 22:08:49 +0530

    Added solution - LeetHub

---
+++ Implement stack using array - GFG/implement-stack-using-array.py
@@ -0,0 +1,47 @@
+#User function Template for python3
+
+class MyStack:
+    
+    def __init__(self):
+        self.arr=[]
+    
+    #Function to push an integer into the stack.
+    def push(self,data):
+        #add code here
+        self.arr.append(data)
+    
+    #Function to remove an item from top of the stack.
+    def pop(self):
+        #add code here
+        if len(self.arr) < 1:
+            return -1
+        temp = self.arr[-1]
+        self.arr = self.arr[:-1]
+        return temp
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        s=MyStack()
+        q=int(input())
+        q1=list(map(int,input().split()))
+        i=0
+        while(i<len(q1)):
+            if(q1[i]==1):
+                s.push(q1[i+1])
+                i=i+2
+            elif(q1[i]==2):
+                print(s.pop(),end=" ")
+                i=i+1
+            elif(s.isEmpty()):
+                print(-1)
+                i=i+1
+        print()   
+# } Driver Code Ends
