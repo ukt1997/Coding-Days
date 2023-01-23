1c8c87d745859dd689844a47a987b5532f4a3744
Ujjwal Kumar <32262314+ukt1997@users.noreply.github.com>
2023-01-23 14:58:12 +0530

    Time: 722 ms (95.82%), Space: 18.9 MB (59.07%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if n == 1:
+            if len(trust) == 0:
+                return 1
+            else:
+                return -1
+        ret = [0 for i in range(n+1)]
+        supporter = []
+        for cur in trust:
+            ret[cur[1]] += 1
+            supporter.append(cur[0])
+        print(ret)
+        max_vote = max(ret)
+        candidate = [i for i,x in enumerate(ret) if x==max_vote] # => [1, 3]
+        print(candidate)
+        if len(candidate) == 1 and max_vote == n - 1 and candidate[0] not in supporter:
+            return candidate[0]
+        return -1
+                
+        
