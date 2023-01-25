fe3b6ce3e23ec18ea66ea1ffeca872f6d6b01bf9
Ujjwal Kumar <32262314+ukt1997@users.noreply.github.com>
2023-01-25 12:08:24 +0530

    Time: 1152 ms (88.51%), Space: 29.2 MB (73.56%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -24,13 +24,21 @@ class Solution:
             n2 = edges[n2]
         
         path = []
+        ret = -1
+        max_dis = 2*len(edges)
         for i in range(len(edges)):
             p1 = node1_path[i]
             p2 = node2_path[i]
             if p1 >= 0 and p2 >= 0:
-                path.append((max(p1,p2) , i))
-        path.sort()
-        #print(path)
+                #path.append((max(p1,p2) , i))
+                if ret == -1:
+                    max_dis = max(p1,p2)
+                    ret = i
+                elif max_dis > max(p1,p2):
+                    max_dis = max(p1,p2)
+                    ret = i
             
-        return -1 if len(path) == 0 else path[0][1]
+        #path.sort()
+            
+        return ret #-1 if len(path) == 0 else path[0][1]
         
