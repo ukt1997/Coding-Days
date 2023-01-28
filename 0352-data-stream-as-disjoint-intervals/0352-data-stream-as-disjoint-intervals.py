48251e2ade65a776e31e072e70dd8e430247c80b
Ujjwal Kumar <32262314+ukt1997@users.noreply.github.com>
2023-01-28 11:46:20 +0530

    Time: 329 ms (42.81%), Space: 19.5 MB (8.36%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
@@ -7,45 +7,41 @@ class SummaryRanges:
 
     def addNum(self, value: int) -> None:
         self.arr.append(value)
-        #print(self.arr)
         
         if (not len(self.sets) ) or  ( len(self.sets) and value < self.sets[0][0] -1 ):
                 # if value is going to be 1st set 
                 self.sets.insert(0,[value,value])
-                #print(self.sets)
                 return
             
         if value > self.sets[len(self.sets) - 1 ][1] + 1 :
                 # if value is going to be last set 
                 self.sets.append([value,value])
-                #print(self.sets)
                 return
         if value == self.sets[len(self.sets) - 1 ][1] + 1 :
-                # if value is going to be last set 
+                # if value is part of last set
                 self.sets[len(self.sets) - 1 ][1] = value
-                #print(self.sets)
                 return
             
         for i in range(len(self.sets)):
             if  self.sets[i][0] <= value <= self.sets[i][1]:
-                #print(self.sets)
+                # already part of any existing set
                 return
             elif self.sets[i][1] + 1 < value < self.sets[i+1][0] - 1:
+                # new set in between cur and next set
                 self.sets.insert(i + 1,[value,value])
-                #print(self.sets)
                 return
             elif self.sets[i][1] + 1 == value == self.sets[i+1][0] - 1:
+                # Merge point of Cur and Next Set
                 next_set = self.sets.pop(i + 1)
                 self.sets[i][1] = next_set[1]
-                #print(self.sets)
                 return
             elif self.sets[i][1] + 1 == value:
+                # Part of cur Set
                 self.sets[i][1] = value
-                #print(self.sets)
                 return
             elif self.sets[i][0] - 1 == value:
+                # part of cur Set
                 self.sets[i][0] = value
-                #print(self.sets)
                 return
                 
             
